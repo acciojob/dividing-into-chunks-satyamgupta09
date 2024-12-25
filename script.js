642b9a5e71e80cbd5d7212c3bdad02dde5fbde15
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2024-12-26 02:49:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let sum=0;
+	let ans=[];
+	let curr=[];
+
+	for(let i=0;i<arr.lenght;i++){
+		if(sum+arr[i]<=n){
+			curr.push(arr[i]);
+			sum=arr[i];
+		}
+		else{
+			ans.push(curr);
+			sum=arr[i];
+			curr=[arr[i]];
+		}
+	}
+	return ans;
 };
 
-const n = prompt("Enter n: ");
+// const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
